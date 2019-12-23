b6e361884a3f35c2386b5fc598f4452097a801f2
darbro96 <dbronikowsk@gmail.com>
2019-12-23 22:58:16 +0100

    Modyfikacja dostępu do /register Prawodpodobnie zewnętrzna baza danych nie obsługuje nativeQuery

--- src/main/java/pl/edu/utp/pralki3/repository/UserRepository.java
+++ src/main/java/pl/edu/utp/pralki3/repository/UserRepository.java
@@ -15,9 +15,9 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("UPDATE User u SET u.password = :newPassword WHERE u.email= :email")
     void updateUserPassword(@Param("newPassword") String password, @Param("email") String email);
 
-    @Query(value = "SELECT * FROM User u ORDER BY u.last_name", nativeQuery = true)
+//    @Query(value = "SELECT * FROM User u ORDER BY u.last_name", nativeQuery = true)
     Page<User> findAll(Pageable pageable);
 
-    @Query(value = "SELECT * FROM User u WHERE u.name LIKE %:param% OR u.last_name LIKE %:param% OR u.email LIKE %:param% ORDER BY u.last_name", nativeQuery = true)
+    @Query(value = "FROM User u WHERE u.name LIKE %:param% OR u.last_name LIKE %:param% OR u.email LIKE %:param% ORDER BY u.last_name", nativeQuery = true)
     Page<User> findAll(Pageable pageable, @Param("param") String param);
 }
